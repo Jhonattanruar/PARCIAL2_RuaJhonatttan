b7bf39be7781a305e63bf9c32b95574cf2223027
JHONATTAN ALBERTO RUA RUIZ <jhonattanrua258527@correo.itm.edu.co>
2023-10-07 20:39:29 -0500

    Entity CREATION

---
+++ PARCIAL2_RuaJhonattan/DAL/Entities/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace PARCIAL2_RuaJhonattan.DAL.Entities
+{
+    public class Entity
+    {
+        public Guid Id { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? ModiedDate { get; set; }
+    }
+}
