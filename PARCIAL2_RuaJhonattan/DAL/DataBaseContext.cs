b02995720caac2ee9517aa38d1471d86d211d942
JHONATTAN ALBERTO RUA RUIZ <jhonattanrua258527@correo.itm.edu.co>
2023-10-07 21:10:17 -0500

    create DbContext

---
+++ PARCIAL2_RuaJhonattan/DAL/DataBaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PARCIAL2_RuaJhonattan.DAL.Entities;
+
+namespace PARCIAL2_RuaJhonattan.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext>option): base(option)
+        {
+            
+        }
+        public DbSet<PersonaNatural> PersonaNaturals { get; set; }
+        
+    }
+}
