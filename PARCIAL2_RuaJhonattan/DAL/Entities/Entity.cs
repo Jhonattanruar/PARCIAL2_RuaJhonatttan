ef913afa662ba9cb6ed152810f9b74c2a8a78d09
JHONATTAN ALBERTO RUA RUIZ <jhonattanrua258527@correo.itm.edu.co>
2023-10-07 20:54:56 -0500

    Creation of "NaturalPerson" table

--- PARCIAL2_RuaJhonattan/DAL/Entities/Entity.cs
+++ PARCIAL2_RuaJhonattan/DAL/Entities/Entity.cs
@@ -1,9 +1,14 @@
-﻿namespace PARCIAL2_RuaJhonattan.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PARCIAL2_RuaJhonattan.DAL.Entities
 {
     public class Entity
     {
+        [Required]
         public Guid Id { get; set; }
+        [Display(Name ="Fecha de creacion")]
         public DateTime? CreatedDate { get; set; }
+        [Display(Name = "Fecha de modificacion")]
         public DateTime? ModiedDate { get; set; }
     }
 }
---
+++ PARCIAL2_RuaJhonattan/DAL/Entities/PersonaNatural.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace PARCIAL2_RuaJhonattan.DAL.Entities
+{
+    public class PersonaNatural : Entity
+    {
+        [Display(Name ="Full Name")]
+        [MaxLength(100, ErrorMessage="El campo {0} debe tener maximo {1} caracteres.")]
+        [Required(ErrorMessage = "¡El campo {0} es requerido!")]
+
+        public String FullName { get; set; }
+        public String Email { get; set; }
+        public int BirthYear { get; set; }
+        public int Age { get; set; }
+
+    }
+}
