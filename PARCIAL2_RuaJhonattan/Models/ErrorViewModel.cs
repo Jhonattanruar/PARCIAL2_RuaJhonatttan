f310d41e2423a8eb1dc7941d6ab30bbb3788b903
JHONATTAN ALBERTO RUA RUIZ <jhonattanrua258527@correo.itm.edu.co>
2023-10-07 20:14:45 -0500

    ASP.NET PROJECT CREATION (CMV)

---
+++ PARCIAL2_RuaJhonattan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PARCIAL2_RuaJhonattan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
